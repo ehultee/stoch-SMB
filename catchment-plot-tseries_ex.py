808d58750e765eb4e85a74b8b45e6e1aa31d38bf
Lizz Ultee [air] <ehultee@umich.edu>
2021-05-26 17:03:51 -0400

    Update catchment-plot-tseries_ex.py
    Revise `fit_catchment_series` to allow non-seasonal fit (for annual time series) .  Revise plotting and show monthly, annual, and annual AR(n) fits together.

--- catchment-plot-tseries_ex.py
+++ catchment-plot-tseries_ex.py
@@ -30,31 +30,40 @@ def read_catchment_series(fpath, anomaly=False):
     else:
         return catchment_tseries
 
-def fit_catchment_series(tseries, comparison_n=range(1,6), multi_model_mode=True, 
-                         strength_of_fit=False):
+def fit_catchment_series(tseries, which_model, comparison_n=range(1,6), 
+                         seasonal=True):
     bic_per_n = pd.DataFrame(index=comparison_n, columns=model_names)
-    for n in comparison_n:
-        for m in model_names:
-            mod = AutoReg(tseries[m], n, trend='ct', seasonal=True)
-            results = mod.fit()
-            bic_per_n[m][n] = results.bic
     
-    if multi_model_mode:
+    if 'multi' in which_model:  ## allow multi-model mode reporting
         for m in model_names:
-            bic_per_n[m] = pd.to_numeric(bic_per_n[m]) # needed for idxmin
+            for n in comparison_n:
+                mod = AutoReg(tseries[m], n, trend='ct', seasonal=seasonal)
+                results = mod.fit()
+                bic_per_n[m][n] = results.bic
+            bic_per_n[m] = pd.to_numeric(bic_per_n[m])
         best_n = bic_per_n.idxmin().mode()[0]
-
-    return best_n
+    else:
+        for n in comparison_n:
+            mod = AutoReg(tseries[which_model], n, trend='ct', seasonal=seasonal)
+            results = mod.fit()
+            bic_per_n[which_model][n] = results.bic
+        bic_per_n[which_model] = pd.to_numeric(bic_per_n[which_model])
+        best_n = bic_per_n[which_model].idxmin()
+    
+    bic_difference = bic_per_n.transform(lambda x: x-x.min())
+    
+    return best_n, bic_difference
     
 
 mod_fits = {m: [] for m in model_names}
 mod_resids = {m: [] for m in model_names}
 basin_i=101
 ctmt_fpath = glob.glob('/Users/lizz/Documents/GitHub/Data_unsynced/SMBMIP-processed/*-catchment_{}-tseries.csv'.format(basin_i))[0]
-s = read_catchment_series(ctmt_fpath)
-best_n = fit_catchment_series(s)
+s = read_catchment_series(ctmt_fpath, anomaly=False)
+a = s.resample('A').sum()
+best_n, _ = fit_catchment_series(a, which_model='multi', seasonal=False)
 for m in model_names:
-    mod = AutoReg(s[m], best_n, trend='ct', seasonal=True).fit()
+    mod = AutoReg(a[m], best_n, trend='ct', seasonal=False).fit()
     fv = mod.fittedvalues
     r = mod.resid
     mod_fits[m] = fv
@@ -63,45 +72,48 @@ for m in model_names:
 
 ## Plot a single timeseries with AR(n) fit
 colors_w = cm.get_cmap('Blues')(np.linspace(0.2, 1, num=len(model_names)))
-fig2, ax2 = plt.subplots(figsize=(10,4))
+fig, ax = plt.subplots(figsize=(10,4))
 for i,m in enumerate(model_names):
     if 'NHM' in m:
-        ax2.plot(s[m], label=m, color=colors_w[i])
-        ax2.plot(mod_fits[m], color='k', alpha=0.8, marker='d', 
+        ax.plot(a[m], label=m, color=colors_w[i])
+        ax.plot(mod_fits[m], color='k', alpha=0.8, marker='d', 
                   label='AR({}) fit to {}'.format(best_n, m))
     else:
         pass
-ax2.set(xlabel='Year', ylabel='Catchment SMB [mm w.e.]',
+ax.set(xlabel='Year', ylabel='Catchment SMB [mm w.e.]',
         # title='Basin {}, all models'.format(basin_i)
         title='Kangerlussuaq catchment, SMB model and AR(n) fit',
         xticks=(np.datetime64('1980-01-01'), np.datetime64('1990-01-01'),
                 np.datetime64('2000-01-01'), np.datetime64('2010-01-01')),
         xticklabels=(1980,1990,2000,2010)
         )
-ax2.legend(bbox_to_anchor=(1.05, 1.0, 0.3, 0.2), loc='upper left')
+ax.legend(bbox_to_anchor=(1.05, 1.0, 0.3, 0.2), loc='upper left')
 plt.tight_layout()
 plt.show()
 
-fig3, (ax3, ax4) = plt.subplots(2, figsize=(10,6), sharex=True)
+fig1, (ax1, ax2, ax3) = plt.subplots(3, figsize=(10,6), sharex=True)
 for i,m in enumerate(model_names):
-    ax3.plot(s[m], label=m, color=colors_w[i])
+    ax1.plot(s[m], label=m, color=colors_w[i])
+    ax2.plot(a[m], label=m, color=colors_w[i])
     if 'NHM' in m:
-        ax4.plot(s[m], label=m, color=colors_w[i])
-        ax4.plot(mod_fits[m], color='k', alpha=0.8, marker='d', 
+        ax3.plot(a[m], label=m, color=colors_w[i])
+        ax3.plot(mod_fits[m], color='k', alpha=0.8, marker='d', 
                   label='AR({}) fit to {}'.format(best_n, m))
     else:
         pass
-ax3.set(ylabel='Catchment SMB [mm w.e.]', 
+ax1.set(ylabel='Monthly SMB [mm w.e.]', 
         title='Kangerlussuaq catchment, SMB models and AR(n) fit')
-ax4.set(xlabel='Year', ylabel='Catchment SMB [mm w.e.]',
+ax2.set(ylabel='Annual SMB [mm w.e.]')
+ax3.set(xlabel='Year', ylabel='Catchment SMB [mm w.e.]',
         # title='Basin {}, all models'.format(basin_i)
         # title='Kangerlussuaq catchment, SMB model and AR(n) fit',
         xticks=(np.datetime64('1980-01-01'), np.datetime64('1990-01-01'),
                 np.datetime64('2000-01-01'), np.datetime64('2010-01-01')),
         xticklabels=(1980,1990,2000,2010)
         )
+ax1.legend(bbox_to_anchor=(1.05, 1.0, 0.3, 0.2), loc='upper left')
 ax3.legend(bbox_to_anchor=(1.05, 1.0, 0.3, 0.2), loc='upper left')
-ax4.legend(bbox_to_anchor=(1.05, 1.0, 0.3, 0.2), loc='upper left')
+fig1.align_ylabels((ax1, ax2, ax3))
 plt.tight_layout()
 plt.show()
     
