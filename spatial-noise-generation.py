58687c94fec6318d09a8fc292507f1e5b0fbf89f
Lizz Ultee [air] <ehultee@umich.edu>
2021-06-21 11:57:54 -0400

    Plot multiple realizations of noise
    Generate several noise terms with sparse covar-Cholesky method and multiple draws from normal vector.  Plot with a "true" residual for comparison.  Small cleanup and comments on earlier lines.

--- spatial-noise-generation.py
+++ spatial-noise-generation.py
@@ -12,7 +12,8 @@ from sklearn.covariance import GraphicalLassoCV, ledoit_wolf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.ar_model import AutoReg, ar_select_order
+from matplotlib import cm
+from statsmodels.tsa.ar_model import AutoReg
 import scipy.linalg
 import glob
 
@@ -64,8 +65,40 @@ cov_ = gl_model.covariance_
 
 ## Now take the Cholesky decomposition of the sparse cov matrix and generate noise with it
 L = scipy.linalg.cholesky(cov_, lower=True)
-N = np.random.normal(size=np.shape(ar_resids))
+N = np.random.normal(size=np.shape(ar_resids)) # draws from normal dist
 spatial_noise = L @ N ## matrix multiply these two
 
 D = np.diag(np.std(ar_resids,1)) ## diagonal matrix of standard devs
 scaled_noise = D @ L @ N
+
+
+## Plot example time series of resids
+## set matplotlib font size defaults
+SMALL_SIZE = 12
+MEDIUM_SIZE = 14
+BIGGER_SIZE = 16
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+highlight_catchment_name, highlight_catchment_id = 'KANGERLUSSUAQ', 101
+noise_realizations = []
+## Several realizations
+for j in range(100):
+    Nj = np.random.normal(size=np.shape(ar_resids))
+    noise_j = D @ L @ Nj
+    noise_realizations.append(noise_j[highlight_catchment_id])
+noise_colors = cm.get_cmap('Blues')
+
+fig, ax = plt.subplots(1, figsize=(10,6))
+for k in range(len(noise_realizations)):
+    ax.plot(range(1980,2012), noise_realizations[k], 
+            color=noise_colors(k/len(noise_realizations)), alpha=0.5)
+ax.plot(range(1980, 2012), ar_resids[highlight_catchment_id], color='k')
+ax.set(xlabel='Year', ylabel='Catchment SMB residual [mm w.e.]')
+plt.show()
