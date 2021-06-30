037692c798598d78275dee92126471d4e358be8a
Lizz Ultee [air] <ehultee@umich.edu>
2021-06-30 17:49:51 -0400

    Update icecore-ACF.py
     Choose familiar GRIP core (`core_names[3]`) as example to highlight.  Produce larger, higher resolution plots for presentation.

--- icecore-ACF.py
+++ icecore-ACF.py
@@ -11,7 +11,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-# from statsmodels.tsa.ar_model import AutoReg, ar_select_order
 from statsmodels.graphics import tsaplots
 
 
@@ -22,7 +21,7 @@ core_tseries = pd.read_csv(core_accum_fpath, index_col=0, parse_dates=[0])
 core_names = core_tseries.columns
 
 series_to_test = core_tseries
-core_to_test = core_names[0]
+core_to_test = core_names[3]
 
 ## Pre-process data
 anomaly_series = series_to_test - series_to_test.mean()
@@ -35,7 +34,7 @@ ax.set(xlabel='Lag [yr]', ylabel='Autocorr.', title='{} Core'.format(core_to_tes
 plt.show()
 
 ## Plot all 5 cores together
-fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, figsize=(4, 6), 
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, figsize=(10, 12), 
                                               sharex=True, sharey=True, tight_layout=True)
 for c, ax in zip(core_names, (ax1,ax2,ax3,ax4,ax5)):
     try:
@@ -47,7 +46,7 @@ for c, ax in zip(core_names, (ax1,ax2,ax3,ax4,ax5)):
 ax5.set(xlabel='Lag [yr]')
 
 ## Plot 4 cores together - 500 years. Milcent not long enough
-fig, (ax6, ax7, ax8, ax9) = plt.subplots(4, figsize=(4, 6), 
+fig, (ax6, ax7, ax8, ax9) = plt.subplots(4, figsize=(10, 12), 
                                               sharex=True, sharey=True, tight_layout=True)
 for c, ax in zip((name for name in core_names if 'Milcent' not in name), 
                  (ax6,ax7,ax8,ax9)):
@@ -58,3 +57,9 @@ for c, ax in zip((name for name in core_names if 'Milcent' not in name),
     except:
         continue
 ax5.set(xlabel='Lag [yr]')
+
+## Plot with a different coloring when significant
+fig, ax = plt.subplots(figsize=(10,5))
+tsaplots.plot_pacf(test_series, ax=ax, lags=100, zero=False)
+ax.set(xlabel='Lag [yr]', ylabel='Autocorr.', title='{} Core'.format(core_to_test.split()[0]))
+plt.show()
